4d659624e8adcfaaf13d14721e6ea4650e74009c
Anusha-Antony <anushaantony2004@gmail.com>
2025-06-02 18:11:47 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const taskInput = document.getElementById("taskInput");
+const addBtn = document.getElementById("addBtn");
+const taskList = document.getElementById("taskList");
+
+addBtn.addEventListener("click", () => {
+  const taskText = taskInput.value.trim();
+  if (taskText === "") return;
+
+  const taskItem = document.createElement("li");
+  taskItem.className =
+    "list-group-item d-flex justify-content-between align-items-center";
+  taskItem.innerHTML = `<span>${taskText}</span><button class="btn btn-danger btn-sm delete">Delete</button>`;
+
+  taskList.appendChild(taskItem);
+  taskInput.value = "";
+});
+
+taskList.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete")) {
+    let result = confirm("Do you want to delete?");
+    if (result) e.target.parentElement.remove();
+  }
+});
